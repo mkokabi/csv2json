acd5282df7fdcad82bca13eee0565639e8acaba9
Mohsen <mkokabi@yahoo.com>
2021-11-13 16:57:51 +1100

    Adding validation

--- csv2jsonlib/Convertor.cs
+++ csv2jsonlib/Convertor.cs
@@ -14,6 +14,8 @@ namespace csv2jsonlib
     public string DestinationPath { get; set; }
 
     public string DestinationField { get; set; }
+
+    public int? DestinationLen { get ;set; }
   }
   public class InstructionSet
   {
@@ -22,20 +24,20 @@ namespace csv2jsonlib
 
   public class Convertor
   {
-    private List<List<string>> CsvToTable(string csv)
+    private List<string[]> CsvToTable(string csv)
     {
       using (var stringReader = new StringReader(csv))
       using (var csvReader = new CsvReader(stringReader, CultureInfo.InvariantCulture))
       {
-        var result = new List<List<string>>();
+        var result = new List<string[]>();
         while (csvReader.Read())
         {
-          var list = new List<String>();
+          var row = new string[csvReader.Parser.Count];
           for (int i = 0; i < csvReader.Parser.Count; i++)
           {
-              list.Add(csvReader[i]);
+              row[i] = csvReader[i];
           }
-          result.Add(list);
+          result.Add(row);
         }
         return result;
       }
@@ -47,10 +49,13 @@ namespace csv2jsonlib
       T result = new T();
 
       JObject jo = JObject.FromObject(result);
-      int i = 0;
-      var dp = inst.Instructions[i].DestinationPath;
-      foreach (var row in tbl)
+      // int i = 0;
+      var dp = inst.Instructions[0].DestinationPath;
+      var errors = new List<string>();
+      for (var i = 0; i < tbl.Count; i++)
+      // foreach (var row in tbl)
       {
+        var row = tbl[i];
         var lo = new List<JProperty>();
         int j = 0;
         JToken jt = jo.SelectToken(dp);
@@ -58,14 +63,29 @@ namespace csv2jsonlib
         foreach (var cell in row)
         {
           var s = inst.Instructions[j].SourceColumn;
-          var df = inst.Instructions[j++].DestinationField;
+          var df = inst.Instructions[j].DestinationField;
+          var dl = inst.Instructions[j++].DestinationLen;
+          if (dl.HasValue && row[s - 1].Length > dl)
+          {
+            errors.Add($"Length validation in row {i}");
+            continue;
+          }
           JProperty jp = new JProperty(df, row[s - 1]);
           lo.Add(jp);
         }
         ja.Add(new JObject(lo.ToArray()));
       }
+      if (errors.Count > 0)
+      {
+        throw new ConvertException { Errors = errors };
+      }
 
       return JsonConvert.DeserializeObject<T>(jo.ToString());
     }
+  }
+
+  public class ConvertException : Exception
+  {
+    public List<string> Errors { get; set; } = new List<string>();
   }
 }
--- csv2jsontests/ConvertorTests.cs
+++ csv2jsontests/ConvertorTests.cs
@@ -5,15 +5,15 @@ using Xunit;
 
 namespace csv2jsontests
 {
-  public class C
+  public class convertor
   {
     public int P1 { get; set; }
-    public int P2 { get; set; }
+    public string P2 { get; set; }
   }
 
   public class J
   {
-    public List<C> C { get; set; } = new List<C>();
+    public List<convertor> convertor { get; set; } = new List<convertor>();
   }
 
   public class OJ
@@ -26,57 +26,83 @@ namespace csv2jsontests
     [Fact]
     public void OneLevelObject()
     {
-      Convertor c = new Convertor();
+      Convertor convertor = new Convertor();
       var instr = JsonConvert.DeserializeObject<InstructionSet>(@"{
         'Instructions':[
             {
               'SourceColumn':1,
-              'DestinationPath':'$.C',
+              'DestinationPath':'$.convertor',
               'DestinationField':'P1'
             },
             {
               'SourceColumn':2,
-              'DestinationPath':'$.C',
+              'DestinationPath':'$.convertor',
               'DestinationField':'P2'
             }
         ]
       }");
       var csv = @"1,2
 3,4";
-      var j = c.Convert<J>(instr, csv);
-      Assert.Equal(2, j.C.Count);
-      Assert.Equal(1, j.C[0].P1);
-      Assert.Equal(2, j.C[0].P2);
-      Assert.Equal(3, j.C[1].P1);
-      Assert.Equal(4, j.C[1].P2);
+      var j = convertor.Convert<J>(instr, csv);
+      Assert.Equal(2, j.convertor.Count);
+      Assert.Equal(1, j.convertor[0].P1);
+      Assert.Equal("2", j.convertor[0].P2);
+      Assert.Equal(3, j.convertor[1].P1);
+      Assert.Equal("4", j.convertor[1].P2);
     }
 
     [Fact]
     public void TwoLevelObject()
     {
-      Convertor c = new Convertor();
+      Convertor convertor = new Convertor();
       var instr = JsonConvert.DeserializeObject<InstructionSet>(@"{
         'Instructions':[
             {
               'SourceColumn':1,
-              'DestinationPath':'$.J.C',
+              'DestinationPath':'$.J.convertor',
               'DestinationField':'P1'
             },
             {
               'SourceColumn':2,
-              'DestinationPath':'$.J.C',
+              'DestinationPath':'$.J.convertor',
               'DestinationField':'P2'
             }
         ]
       }");
       var csv = @"1,2
 3,4";
-      var oj = c.Convert<OJ>(instr, csv);
-      Assert.Equal(1, oj.J.C[0].P1);
-      Assert.Equal(2, oj.J.C[0].P2);
-      Assert.Equal(3, oj.J.C[1].P1);
-      Assert.Equal(4, oj.J.C[1].P2);
-      Assert.Equal(2, oj.J.C.Count);
+      var oj = convertor.Convert<OJ>(instr, csv);
+      Assert.Equal(2, oj.J.convertor.Count);
+      Assert.Equal(1, oj.J.convertor[0].P1);
+      Assert.Equal("2", oj.J.convertor[0].P2);
+      Assert.Equal(3, oj.J.convertor[1].P1);
+      Assert.Equal("4", oj.J.convertor[1].P2);
+    }
+
+    [Fact]
+    public void TwoLevelObjectWithLenValidation()
+    {
+      Convertor convertor = new Convertor();
+      var instr = JsonConvert.DeserializeObject<InstructionSet>(@"{
+        'Instructions':[
+            {
+              'SourceColumn':1,
+              'DestinationPath':'$.J.convertor',
+              'DestinationField':'P1',
+              'DestinationLen': 2
+            },
+            {
+              'SourceColumn':2,
+              'DestinationPath':'$.J.convertor',
+              'DestinationField':'P2'
+            }
+        ]
+      }");
+      var csv = @"1AA,2
+3,4";
+
+      var exception = Assert.Throws<ConvertException>(() => convertor.Convert<OJ>(instr, csv));
+      Assert.Equal("Length validation in row 0", exception.Errors[0]);
     }
   }
 }
